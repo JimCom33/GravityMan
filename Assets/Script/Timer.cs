c8cc7f4d3d3bc1bdf8395d48423f1beb0bbf6b80
Jimmy Comeault <jimmy.comeault@gmail.com>
2024-10-07 14:25:44 -0400

    Timer

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    TMP_Text timerText;
+    float timePassedInSeconds;
+
+    private void Start()
+    {
+        timerText = GetComponent<TMP_Text>();
+    }
+    void Update()
+    {
+        timePassedInSeconds += Time.deltaTime;
+        timerText.text = $"Timer: {(int)timePassedInSeconds}";
+    }
+}
